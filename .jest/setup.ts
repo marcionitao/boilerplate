8dc237e902c7b886995813840742e9ce14ad7fec
Marcio Nitao <marcionitao@gmail.com>
2022-07-22 17:10:54 +0200

    add Styled Components + PWA

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,5 +1,6 @@
 /**
  * Aqui serão adicionados alguns imports de libs que iremos usar com o jest, por exemplo,
- * recursos da testing-library
+ * recursos da testing-library, styled-components, etc.
  */
 import '@testing-library/jest-dom'
+import 'jest-styled-components'
